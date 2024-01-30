b32b36ae809d8bce5a5c56dadc80f3cd6c8a5b0f
ObaidAshiq <o.ashiq@tachyonsecurity.eu>
2024-01-30 22:03:43 +0530

    TypeError: fetch failed fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,32 +3,35 @@ import PostCard from "../Components/postCard/postCard";
 import styles from "./blog.module.css";
 
 // FETCH DATA WITH AN API
-const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+// const getData = async () => {
+//   const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
 
-  if (!res.ok) {
-    throw new Error("Something went wrong");
-  }
+//   if (!res.ok) {
+//     throw new Error("Something went wrong");
+//   }
 
-  return res.json();
-};
+//   return res.json();
+// };
 
 const BlogPage = async () => {
 
   // FETCH DATA WITH AN API
-  const posts = await getData();
+  // const posts = await getData();
 
   // FETCH DATA WITHOUT AN API
   // const posts = await getPosts();
 
   return (
-    <div className={styles.container}>
+    <>
+    {/* <div className={styles.container}>
       {posts.map((post:any) => (
         <div className={styles.post} key={post.id}>
           <PostCard post={post} />
         </div>
       ))}
-    </div>
+    </div> */}
+    <div>Blog page</div>
+    </>
   );
 };
 
