1100b4dfd511ae1782409ccc95f4790a65df29ac
ObaidAshiq <o.ashiq@tachyonsecurity.eu>
2024-02-20 09:21:13 +0530

    test opensource

--- app/(services)/carRental/page.tsx
+++ app/(services)/carRental/page.tsx
@@ -6,6 +6,7 @@ const CarRentalPage = () => {
     <div>
         <div className='w-full flex items-center justify-center'>
         <Heading heading={'Our Impressive Collection of Cars'} subHeading={'Ranging from elegant sedans to powerful sports car,all carefully selected to provide our customers with the ultimate driving experience'}/>
+        Test
         </div>
     </div>
   )
